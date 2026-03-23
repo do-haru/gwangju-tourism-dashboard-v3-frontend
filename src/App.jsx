5c0eaff875bbcf313be090874ca94c0a21339e0f
do-haru <doharu05@naver.com>
2026-03-23 14:44:48 +0900

    feat: 레이아웃 컴포넌트(Header, Main) 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <>gwangju-tourism-dashboard</>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
