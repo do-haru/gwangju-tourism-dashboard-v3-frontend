b0b2a37101a0db98f3fcaa144fdb7fa4c2339346
do-haru <doharu05@naver.com>
2026-03-24 16:38:59 +0900

    styel: 스타일 정리

--- src/components/Chart1.jsx
+++ src/components/Chart1.jsx
@@ -14,34 +14,53 @@ import {
 const Chart1 = ({ trendData, summary }) => {
   return (
     <div className="Chart1">
-      {/* LineChart (추세) */}
-      <LineChart width={700} height={300} data={trendData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="total" />
-      </LineChart>
-      {/* BarChart (증가/감소) */}
-      <BarChart width={700} height={300} data={trendData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Bar dataKey="diff" />
-      </BarChart>
+      <div className="Chart1_1">
+        {/* LineChart (추세) */}
+        <h3>일별 방문자 수 추세</h3>
+        <LineChart width={700} height={300} data={trendData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="total" />
+        </LineChart>
+        <p className="chart-desc">
+          ※날짜별 전체 방문자 수의 변화를 나타내는 그래프
+        </p>
+      </div>
+      <div className="Chart1_2">
+        {/* BarChart (증가/감소) */}
+        <h3>일별 방문자 증감</h3>
+        <BarChart width={700} height={300} data={trendData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="diff" />
+        </BarChart>
+        <p className="chart-desc">
+          ※전일 대비 방문자 수의 증가 및 감소를 나타내는 그래프
+        </p>
+      </div>
       {/* 방문자수 최대, 최소, 평균 지표 */}
       {summary && (
-        <div className="summary">
-          <p>
-            최대 방문일: {summary.max.date} (
-            {summary.max.total.toLocaleString()}명)
-          </p>
-          <p>
-            최소 방문일: {summary.min.date} (
-            {summary.min.total.toLocaleString()}명)
-          </p>
-          <p>평균 방문자: {summary.avg.toLocaleString()}명</p>
+        <div className="Chart1_3">
+          <div className="summary-card">
+            <p className="label">최대 방문일</p>
+            <p className="value">{summary.max.date}</p>
+            <p className="sub">{summary.max.total.toLocaleString()}명</p>
+          </div>
+
+          <div className="summary-card">
+            <p className="label">최소 방문일</p>
+            <p className="value">{summary.min.date}</p>
+            <p className="sub">{summary.min.total.toLocaleString()}명</p>
+          </div>
+
+          <div className="summary-card">
+            <p className="label">평균 방문자</p>
+            <p className="value">{summary.avg.toLocaleString()}명</p>
+          </div>
         </div>
       )}
     </div>
