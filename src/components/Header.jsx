97aa4016fa162031271acbec52d27dc7fceffdcd
do-haru <doharu05@naver.com>
2026-03-25 14:17:23 +0900

    style: Header 스타일 개선

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="Header">
+      <p className="dashboard-title">동구 행정동별 방문자 분석 대시보드</p>
+    </div>
+  );
 };
 
 export default Header;
